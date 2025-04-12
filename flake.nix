129e5ca84b68fec831c1914d31c32139f1191f17
Victor Luque <luque.viictor@gmail.com>
2025-04-12 20:11:43 +0200

    add home-manager

--- flake.nix
+++ flake.nix
@@ -4,21 +4,43 @@
   inputs = {
     # NixOS official package source, using the nixos-24.05 branch.
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    # Home Manager for managing user configuration.
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.shunya-dsktp = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect.
-        ./configuration.nix
-        {
-           nix = {
-              settings.experimental-features = [ "nix-command" "flakes" ];
-           };
-        }
-      ];
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      shunya-dsktp = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          {
+             nix = {
+                settings.experimental-features = [ "nix-command" "flakes" ];
+             };
+          }
+
+          # Make home-manager a module of nixos, so that its configuration
+          # configuration will be deployed automatically when rebuilding nixos.
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.shunya = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
     };
   };
 }
