36579f0d0c3c56f7786633c090f4b3c04a12efb8
Victor Luque <luque.viictor@gmail.com>
2025-04-12 20:26:14 +0200

    flake.nix

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.shunya = import ./home.nix;
+            home-manager.users.shunya = import ./home/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
