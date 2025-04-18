a66a64aa0b2f8d422f31a7906dbbd4fbd9999eb5
Victor Luque <luque.viictor@gmail.com>
2025-04-18 10:54:46 +0200

    cleanup home-manager

--- home/shared/default.nix
+++ home/shared/default.nix
@@ -17,44 +17,29 @@
   # '';
 
   # set cursor size and dpi for 4k monitor
-  xresources.properties = {
-    "Xcursor.size" = 16;
-    "Xft.dpi" = 172;
-  };
+  # xresources.properties = {
+  #   "Xcursor.size" = 16;
+  #   "Xft.dpi" = 172;
+  # };
 
-  # Packages that should be installed to the user profile.
+  # Packages that should be installed to all user profiles.
   home.packages = with pkgs; [
-    # here is some command line tools I use frequently
-    # feel free to add your own or remove some of them
 
+    # Command line tools.
     neofetch
-    nnn # terminal file manager
 
-    # archives
+    # Archives.
     zip
-    xz
     unzip
-    p7zip
+    xz
 
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processor https://github.com/mikefarah/yq
-    eza # A modern replacement for ‘ls’
-    fzf # A command-line fuzzy finder
+    # Networking.
+    mtr       		# Network diagnostic tool.
+    dnsutils  		# `dig` + `nslookup`.
+    nmap      		# Utility for network discovery and security auditing.
+    ipcalc    		# Calculator for the IPv4/v6 addresses.
 
-    # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils  # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc  # it is a calculator for the IPv4/v6 addresses
-
-    # misc
-    cowsay
+    # Misc.
     file
     which
     tree
@@ -64,31 +49,22 @@
     zstd
     gnupg
 
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
-    nix-output-monitor
+    # Nix.
+    nix-output-monitor 	# `nom` works like `nix` with more detailed output.
 
-    # productivity
-    hugo # static site generator
-    glow # markdown previewer in terminal
+    # Productivity
+    glow 		# Markdown previewer in terminal.
 
-    btop  # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
+    # System call monitoring.
+    strace 		# System call monitoring.
+    ltrace 		# Library call monitoring.
+    lsof 		# List open files.
 
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
-
-    # system tools
+    # System tools.
     sysstat
-    lm_sensors # for `sensors` command
     ethtool
-    pciutils # lspci
-    usbutils # lsusb
+    pciutils 		# `lspci`
+    usbutils 		# `lsusb`
   ];
 
   # starship - an customizable prompt for any shell
@@ -100,21 +76,6 @@
       aws.disabled = true;
       gcloud.disabled = true;
       line_break.disabled = true;
-    };
-  };
-
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
-  programs.alacritty = {
-    enable = true;
-    # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
     };
   };
 
